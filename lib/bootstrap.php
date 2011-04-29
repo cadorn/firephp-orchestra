e0dbd9840baef3a11b4439a3a8dd1433fe0825a7
cadorn <christoph@christophdorn.com>
2011-04-29 11:25:33 -0700

    Added firephp.phar and load FirePHP in bootstrap

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello World!";
+/**
+ * Load FirePHP
+ */
+
+define('INSIGHT_IPS', '*');
+define('INSIGHT_AUTHKEYS', '*');
+define('INSIGHT_PATHS', dirname(__FILE__));
+define('INSIGHT_SERVER_PATH', '/');
+
+require_once('phar://firephp.phar/FirePHP/Init.php');
+
+/**
+ * Application Code
+ */
+
+echo 'Hello World!';
+
+// @see http://reference.developercompanion.com/#/Tools/FirePHPCompanion/API/
+FirePHP::to('request')->console('Dev')->log('Hello World');
